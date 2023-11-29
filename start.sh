1d5e55357d08f5bdcbc2fe127b7525168a2d13e7
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-11-29 18:40:19 +0200

    start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR2yLZGJV6e7zKJqx58fuoGSy4bedfp9WFhCZv6HJHXVWdzVapGPkaAHKUSdAxhWgJFwz7sDj5p2Rdvv7zcaW9ziHDL1oHW2p -p ZZeus -a rx/0 -k 
+pause
